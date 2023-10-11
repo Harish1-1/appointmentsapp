3f1a997e367f1f987c29371d007dd72578af5a3e
Harish1-1 <gundaharishkumar2@gmail.com>
2023-10-11 13:34:57 +0000

    commit message

--- src/components/AppointmentItem/index.js
+++ src/components/AppointmentItem/index.js
@@ -16,12 +16,7 @@ const AppointmentIem = props => {
     <li className="appointment-item">
       <div className="header-container">
         <p className="title">{title}</p>
-        <button
-          type="button"
-          className="star-button"
-          onClick={onClickStar}
-          testid="star"
-        >
+        <button type="button" className="star-button" onClick={onClickStar}>
           <img src={starImgUrl} className="star" alt="star" />
         </button>
       </div>
